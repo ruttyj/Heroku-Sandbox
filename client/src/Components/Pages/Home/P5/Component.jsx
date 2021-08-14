b3428bbeb4db3efc3da7cc567bf91660e0b7902e
RuttyJ <ruttyj92@hotmail.com>
2021-08-14 13:46:51 -0400

    Toggle Confetti

--- client/src/Components/Pages/Home/P5/Component.jsx
+++ client/src/Components/Pages/Home/P5/Component.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Sketch from 'react-p5';
 
 // Attempting to reproduct https://codepen.io/Gthibaud/pen/ENzXbp
@@ -16,6 +16,9 @@ class Particle {
     this.y = 0;
     this.x = 0;
 
+    this.mActive = true;
+    this.mDead = false;
+
     this.parent = parent;
     this.gravite = p5.createVector(0, 0.1);//parent.gravite;
     this.reinit(p5);
@@ -28,17 +31,44 @@ class Particle {
     this.priseVitesse = 0.05;
   }
 
+  _setDead(val)
+  {
+    this.mDead = val;
+  }
+
+  isDead()
+  {
+    return this.mDead;
+  }
+
+  setActive(val)
+  {
+    this.mActive = val;
+    if (val) {
+      this._setDead(false);
+    }
+  }
+
+  isActive()
+  {
+    return this.mActive;
+  }
+
   reinit(p5)
   {
-    this.y = 0; 
-    this.position = p5.createVector(0, 0);//this.parent.position.copy();
-    this.position.y = p5.random(-20, -100);
-    this.position.x = p5.random(0, p5.width);
-    this.velocite = p5.createVector(p5.random(-6, 6), p5.random(-10, 2));
-    this.friction = p5.random(0.995, 0.98);
-    this.taille = p5.round(p5.random(5, 15));
-    this.moitie = this.taille / 2;
-    this.couleur = p5.color(p5.random(themeColors));
+    if(this.isActive()){
+      this.y = 0; 
+      this.position = p5.createVector(0, 0);//this.parent.position.copy();
+      this.position.y = p5.random(-20, -100);
+      this.position.x = p5.random(0, p5.width);
+      this.velocite = p5.createVector(p5.random(-6, 6), p5.random(-10, 2));
+      this.friction = p5.random(0.995, 0.98);
+      this.taille = p5.round(p5.random(5, 15));
+      this.moitie = this.taille / 2;
+      this.couleur = p5.color(p5.random(themeColors));
+    } else {
+      this._setDead(true);
+    }
   }
 
   update(p5)
@@ -61,160 +91,115 @@ class Particle {
 
   draw(p5)
   {
-    this.etape = 0.5 + Math.sin(this.velocite.y * 20) * 0.5;
-
-    this.prise = this.priseFacteur + Math.cos(this.priseAngle) * this.multFacteur;
-    this.priseAngle += this.priseVitesse;
-    p5.translate(this.position.x, this.position.y);
-    p5.rotate(this.velocite.x * 2);
-    p5.scale(1, this.etape);
-    p5.noStroke();
-    p5.fill(this.couleur);
-
-    if (this.forme === 0) {
-      p5.rect(-this.moitie, -this.moitie, this.taille, this.taille);
-    } else {
-      p5.ellipse(0, 0, this.taille, this.taille);
+    if(!this.isDead()) {
+      this.etape = 0.5 + Math.sin(this.velocite.y * 20) * 0.5;
+      this.prise = this.priseFacteur + Math.cos(this.priseAngle) * this.multFacteur;
+      this.priseAngle += this.priseVitesse;
+      p5.translate(this.position.x, this.position.y);
+      p5.rotate(this.velocite.x * 2);
+      p5.scale(1, this.etape);
+      p5.noStroke();
+      p5.fill(this.couleur);
+  
+      if (this.forme === 0) {
+        p5.rect(-this.moitie, -this.moitie, this.taille, this.taille);
+      } else {
+        p5.ellipse(0, 0, this.taille, this.taille);
+      }
+  
+      p5.resetMatrix();
     }
-
-    p5.resetMatrix();
   }
 
 }
 
-class ParticleSystem 
-{
-  constructor(p5, parent) {
-    this.y = 0;
-    this.x = 0;
-
-    this.parent = parent;
-    this.gravite = p5.createVector(0, 0.1);//parent.gravite;
-    this.reinit();
-    this.forme = p5.round(p5.random(0, 1));
-    this.etape = 0;
-    this.prise = 0;
-    this.priseFacteur = p5.random(-0.02, 0.02);
-    this.multFacteur = p5.random(0.01, 0.08);
-    this.priseAngle = 0;
-    this.priseVitesse = 0.05;
+class ParticleSystem {
+  constructor(p5, maxNumber, position, gravity) {
+    //-----------------------------------------
+    this.isActive = true;
+    this.position = position.copy();
+    this.maxNumber = maxNumber;
+    this.gravity = gravity;
+    this.friction = 0.98;
+    this.particles = [];
+    for (var i = 0; i < this.maxNumber; i++) {
+      this.particles.push(new Particle(p5, this));
+    }
   }
 
-  reinit(p5)
+  setActive(val)
   {
-    this.y = 0; 
+    this.isActive = val;
+    this.particles.forEach(particle => {
+      particle.setActive(this.isActive);
+    })
+  }
 
-    this.position = this.parent.position.copy();
-    this.position.y = p5.random(-20, -100);
-    this.position.x = p5.random(0, p5.width);
-    this.velocite = p5.createVector(p5.random(-6, 6), p5.random(-10, 2));
-    this.friction = p5.random(0.995, 0.98);
-    this.taille = p5.round(p5.random(5, 15));
-    this.moitie = this.taille / 2;
-    this.couleur = p5.color(p5.random(themeColors));
+  isActive()
+  {
+    return this.isActive;
   }
 
   update(p5)
   {
-    if (this.y > p5.height) {
-      this.reinit(p5);
-    }
-    else {
-      this.y += 8;
-    }
-
-    this.velocite.add(this.gravite);
-    this.velocite.x += this.prise;
-    this.velocite.mult(this.friction);
-    this.position.add(this.velocite);
-    if (this.position.y > p5.height) {
-      this.reinit();
-    }
-
-    if (this.position.x < 0) {
-      this.reinit();
-    }
-    if (this.position.x > p5.width + 10) {
-      this.reinit();
-    }
+    this.particles.forEach(particle => {
+      particle.setActive(this.isActive);
+      particle.update(p5);
+    })
   }
 
   draw(p5)
   {
-    let y = this.y;
-    let x = this.x;
-    let color = [255, y * 1.3, 0];
-    let position = [x, y];
-    let size = 7;
-
-
-    p5.fill(color);
-    p5.rect(...position, size, size);
-
-
-    
-    this.etape = 0.5 + Math.sin(this.velocite.y * 20) * 0.5;
-
-    this.prise = this.priseFacteur + Math.cos(this.priseAngle) * this.multFacteur;
-    this.priseAngle += this.priseVitesse;
-    p5.translate(this.position.x, this.position.y);
-    p5.rotate(this.velocite.x * 2);
-    p5.scale(1, this.etape);
-    p5.noStroke();
-    p5.fill(this.couleur);
-
-    if (this.forme === 0) {
-      p5.rect(-this.moitie, -this.moitie, this.taille, this.taille);
-    } else {
-      p5.ellipse(0, 0, this.taille, this.taille);
-    }
-
-    p5.resetMatrix();
+    this.particles.forEach(particle => {
+      particle.draw(p5);
+    })
   }
 
 }
 
 function useParticleSystem() {
-  const y = useRef(0);
-  const mParticles = useRef([]);
+  const mConfetti = useRef();
+  const [mActive, mSetIsActive] = useState(true);
 
- 
   function setup(p5)
   {
-    mParticles.current = [];
-    for (let i=0; i< 200; ++i) {
-      mParticles.current.push(makeParticle(p5));
-    }
-  }
-
-  function makeParticle(p5)
-  {
-    let particle = new Particle(p5);
-    particle.y = 0;
-    particle.x = p5.random(0, p5.width);
-
-    return particle;
+    const gravity = p5.createVector(0, 0.1);
+    const position = p5.createVector(p5.width / 2, -20);
+    mConfetti.current = new ParticleSystem(p5, 500, position, gravity);
+    mConfetti.current.setActive(mActive);
   }
 
   function update(p5)
   {
-    getParticles().forEach(particle => {
-      particle.update(p5);
-    })
+    getConfetti().update(p5);
   }
 
   function draw(p5)
   {
-    update(p5);
     p5.clear();
-    getParticles().forEach(particle => {
-      particle.draw(p5);
-    })
+    getConfetti().draw(p5);
   }
 
-  function getParticles()
+  function getConfetti()
   {
-    return mParticles.current;
+    return mConfetti.current;
+  }
+
+  function setIsActive(value)
+  {
+    mSetIsActive(value);
+    mConfetti.current.setActive(value);
+  }
+
+  function render(p5)
+  {
+    update(p5);
+    draw(p5);
+  }
+
+  function getIsActive()
+  {
+    return mActive;
   }
 
 
@@ -224,6 +209,10 @@ function useParticleSystem() {
       setup,
       update,
       draw,
+      render,
+      //------------------
+      getIsActive,
+      setIsActive,
     }
   }
   
@@ -243,9 +232,12 @@ export default () => {
   };
 
 
+  let isSystemActive = particleSystem.getIsActive();
+  const toggleSystem = () => particleSystem.setIsActive(!isSystemActive);
   return (
     <div>
-      <Sketch setup={setup} draw={particleSystem.draw} />
+      <Sketch setup={setup} draw={particleSystem.render} />
+      <button onClick={toggleSystem}>{isSystemActive ? "System On" : "System Off"}</button>
     </div>
   );
 }
